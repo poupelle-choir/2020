b2ac87b508755c20ef0d960a144d81afaa002ac3
poupelle-choir <69413319+poupelle-choir@users.noreply.github.com>
2020-08-30 13:35:17 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -150,16 +150,5 @@ $(function (){
   });
 });
 
-
-// スマホを判定する処理
-function isMobile() {
-  const ua = navigator.userAgent;
-  return ua.indexOf('iPhone') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0
-}
-
-// スマホのときにbodyタグにクラスをつける
-if (isMobile()) {
-  document.body.classList.add('is-mobile');
-}
   
 
