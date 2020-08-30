cdac31980b49cb0cbb94eea4f14a89dd0cfbf065
poupelle-choir <69413319+poupelle-choir@users.noreply.github.com>
2020-08-30 15:26:48 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,14 +4,14 @@
 
 
 // スマホ横向き
-  $(window).on('load orientationchange resize', function(){
-    if (Math.abs(window.orientation) === 90) {
-      // 横向きになったときの処理
-      $('body').toggleClass('is-mobile');
-    } else {
-      // 縦向きになったときの処理
-      $('body').removeClass('is-mobile');
-    }
+$(window).on('load orientationchange resize', function(){
+  if (Math.abs(window.orientation) === 90) {
+    // 横向きになったときの処理
+    $('.rotate').show();
+  } else {
+    // 縦向きになったときの処理
+    $('.rotate').hide();
+  }
 });
 
 
