832362c05cff5c9c53a8e22048c3ce6a636d3cbc
poupelle-choir <69413319+poupelle-choir@users.noreply.github.com>
2020-09-02 14:32:40 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,24 +95,24 @@ $(function () {
 
 
 
-/*--------------------------------
-  * 背景固定部分
----------------------------------*/
-$(function () {
-  var target1 = $("#parallax-bg");
-  var targetPosOT1 = target1.offset().top;
-  var targetFactor = 0.4;
-  var windowH = $(window).height();
-  var scrollYStart1 = targetPosOT1 - windowH;
-  $(window).on('scroll',function(){
-    var scrollY = $(this).scrollTop();
-    if(scrollY > scrollYStart1){
-      target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
-    }else{
-      target1.css('background-position','center top');
-    }
-  });
-});
+// /*--------------------------------
+//   * 背景固定部分
+// ---------------------------------*/
+// $(function () {
+//   var target1 = $("#parallax-bg");
+//   var targetPosOT1 = target1.offset().top;
+//   var targetFactor = 0.4;
+//   var windowH = $(window).height();
+//   var scrollYStart1 = targetPosOT1 - windowH;
+//   $(window).on('scroll',function(){
+//     var scrollY = $(this).scrollTop();
+//     if(scrollY > scrollYStart1){
+//       target1.css('background-position-y', (scrollY - targetPosOT1) * targetFactor + 'px');
+//     }else{
+//       target1.css('background-position','center top');
+//     }
+//   });
+// });
 
 
 
