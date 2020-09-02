c24ede3c99a1980f253feb59f45c5a32cf7cc60b
poupelle-choir <69413319+poupelle-choir@users.noreply.github.com>
2020-09-02 15:36:05 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,22 +54,22 @@ $(function () {
   }); 
 });
 
-var state = false;
-var scrollpos;
+// var state = false;
+// var scrollpos;
 
-$('#menu-bar').on("touchstart",function () {
-  if (state == false) {
-    scrollpos = $(window).scrollTop();
-    $('body').addClass('fixed').css({'top': -scrollpos});
-    $('#menu-bar').addClass('open');
-    state = true;
-  } else {
-    $('body').removeClass('fixed').css({'top': 0});
-    window.scrollTo( 0 , scrollpos );
-    $('#menu-bar').removeClass('open');
-    state = false;
-  }
-});
+// $('#menu-bar').on("touchstart",function () {
+//   if (state == false) {
+//     scrollpos = $(window).scrollTop();
+//     $('body').addClass('fixed').css({'top': -scrollpos});
+//     $('#menu-bar').addClass('open');
+//     state = true;
+//   } else {
+//     $('body').removeClass('fixed').css({'top': 0});
+//     window.scrollTo( 0 , scrollpos );
+//     $('#menu-bar').removeClass('open');
+//     state = false;
+//   }
+// });
 
 
 /*--------------------------------
