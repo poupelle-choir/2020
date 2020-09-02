dc6b51ab6c4158d9d3c8c7143f0e3e5c0a7c4711
poupelle-choir <69413319+poupelle-choir@users.noreply.github.com>
2020-09-02 15:56:38 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -155,17 +155,17 @@ $(function(){
 });
 
 
-// $(window).on('load', function() {
-//  var url = $(location).attr('href');
-//  if(url.indexOf("?id=") != -1){
-//    var id = url.split("?id=");
-//    var $target = $('#' + id[id.length - 1]);
-//    if($target.length){
-//      var pos = $target.offset().top;
-//      $("html, body").animate({scrollTop:pos}, 1500);
-//    }
-//  }
-// });
+$(window).on('load', function() {
+ var url = $(location).attr('href');
+ if(url.indexOf("?id=") != -1){
+   var id = url.split("?id=");
+   var $target = $('#' + id[id.length - 1]);
+   if($target.length){
+     var pos = $target.offset().top;
+     $("html, body").animate({scrollTop:pos}, 1500);
+   }
+ }
+});
 
 
 /*--------------------------------
